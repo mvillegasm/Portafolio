02a7b72aa9fbb50ed28e84bf7e8790f949ff555c
Marcos Villegas <dinavers@gmail.com>
2019-05-02 11:07:33 -0400

    smooth scroll config

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== ""){
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+});
